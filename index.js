688b96c4db6c54d57772bfef88930d09d017686a
Rene Saarsoo <nene@triin.net>
2014-12-12 22:36:50 +0200

    Implement formatting.
    Converting of the AST back to string.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var Parser = require("./lib/Parser");
+var Formatter = require("./lib/Formatter");
 
 module.exports = {
     parse: function(text) {
         return new Parser(text).parse();
+    },
+
+    format: function(parts) {
+        return new Formatter(parts).format();
     }
 };
